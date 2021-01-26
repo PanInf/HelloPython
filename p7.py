4ca999aa938604645b69d8ee596215178d80d12f
PanInfVSC <paninf@paninf.pl>
2021-01-26 09:57:48 +0100

    change L i M

--- p7.py
+++ p7.py
@@ -1,5 +1,11 @@
 from random import randint
 
+# tworzenie normalnej listy (jak dotychczas)
+L = [randint(1,100) for i in range(5)]
+
+# tworzenie list w liście
 M = [[randint(1,100) for i in range(5)] for j in range(5)]
 
+print(L)
 print(M)
+
