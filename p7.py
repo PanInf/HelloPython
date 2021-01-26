ca18b4f4658535c6764971d2719a852dfc6f56f1
PanInfVSC <paninf@paninf.pl>
2021-01-26 10:00:00 +0100

    Zadania na rozgrzwkę

--- p7.py
+++ p7.py
@@ -9,3 +9,9 @@ M = [[randint(1,100) for i in range(5)] for j in range(5)]
 print(L)
 print(M)
 
+# wypisanie 2 i 3 listy w macierzy
+
+# wypisanie pierwszego elementu ostatniej listy w macierzy
+
+# wypisanie ostatniego elementu pierwszej listy w macierzy
+
