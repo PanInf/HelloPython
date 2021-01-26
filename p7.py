a02298a03517cb2c0da53141cf8d13866e40fb36
PanInfVSC <paninf@paninf.pl>
2021-01-26 10:10:33 +0100

    Dodanie zestawu pytań

--- p7.py
+++ p7.py
@@ -1,12 +1,12 @@
 from random import randint
 
 # tworzenie normalnej listy (jak dotychczas)
-L = [randint(1,100) for i in range(5)]
+# L = [randint(1,100) for i in range(5)]
 
-# tworzenie list w liście
+# tworzenie list w liście (macierz)
 M = [[randint(1,100) for i in range(5)] for j in range(5)]
 
-print(L)
+# print(L)
 print(M)
 
 # wypisanie 2 i 3 listy w macierzy
@@ -15,3 +15,12 @@ print(M)
 
 # wypisanie ostatniego elementu pierwszej listy w macierzy
 
+# wypisanie sumy elementów w pierwszej liście macierzy
+
+# wypisanie sumy wszystkich elementów w macierzy
+
+# wybrane największego elementu w macierzy
+
+# wypisanie tych list macierzy, które kończą się liczbą parzystą
+
+# wypisanie tej listy z macierzy, która ma najwiekszą sumę elementów
