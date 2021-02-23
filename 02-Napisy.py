f3f038c5db3069f97b064ded295fc3873edd9fa2
PanInfVSC <paninf@paninf.pl>
2021-02-23 11:06:43 +0100

    Adding cezar

--- 02-Napisy.py
+++ 02-Napisy.py
@@ -1,7 +1,37 @@
-litera = "a"
-print(litera)
-print(ord(litera))
-print(chr(97))
+def cezar(n, k):
+    szyfr = ""
+    for i in n:
+        szyfr += chr(((ord(i)+k-65)%26)+65)
+    return szyfr
+
+def antycezar(n, k):
+    slowo = ""
+    for i in n:
+        slowo += chr(((ord(i)-k-65)%26)+65)
+    return slowo
+
+print(cezar("WXY", 3))
+print(antycezar("ZAB", 3))
+
+# SLOWO = "WXY"
+
+# x=3
+# SZYFR = ""
+# for i in SLOWO:
+#     SZYFR += chr(((ord(i)+x-65)%26)+65)
+# print(SZYFR)
+
+#EDQN
+
+# for i in range(65, 91):
+#     print(chr(i), chr(i+32), end=" ")
+
+# print(chr(77))
+
+# litera = "a"
+# print(litera)
+# print(ord(litera))
+# print(chr(97))
 
 # tekst = " Litwo! Ojczyzno moja. Ty jesteś jak zdrowie."
 
