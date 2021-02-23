0fa308ffe831da7d03e959e85e8ce7dc7a88f672
PanInfVSC <paninf@paninf.pl>
2021-02-23 13:59:32 +0100

    Change 02-string

--- 02-Napisy.py
+++ 02-Napisy.py
@@ -13,49 +13,76 @@ def antycezar(n, k):
 print(cezar("WXY", 3))
 print(antycezar("ZAB", 3))
 
-# SLOWO = "WXY"
 
-# x=3
-# SZYFR = ""
+# SLOWO = "BANK"
+
 # for i in SLOWO:
-#     SZYFR += chr(((ord(i)+x-65)%26)+65)
-# print(SZYFR)
+#     print(chr(ord(i)+3), end="")  # EDQN
 
-#EDQN
+
+# chr() ord() 
+
+# print(chr(70))
+# print(ord("G"))
 
 # for i in range(65, 91):
-#     print(chr(i), chr(i+32), end=" ")
+#     print(chr(i+1), end=" ")
 
-# print(chr(77))
+# print()
 
-# litera = "a"
-# print(litera)
-# print(ord(litera))
-# print(chr(97))
+# alfabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-# tekst = " Litwo! Ojczyzno moja. Ty jesteś jak zdrowie."
+# for i in alfabet:
+#     print(ord(i), end=" ")
 
-# print(tekst.upper())  # MATURA
-# print(tekst.lower())  # MATURA
-# print(tekst.capitalize())
-# print(tekst.strip())  #lstrip rstrip  MATURA
-# print(tekst.replace("a", "-xxx-"))
-# print(tekst.split())  # MATURA
-# print(tekst.split(sep="."))
-# print(tekst.replace("!", ".").split(sep="."))
 
-# napis = "Ala ma kota." 
-# print(napis[2])
-# print(napis[2:8])
-# print(len(napis))
+# L = ["Ala", "ma", "kota"]
+# print(" ".join(L))  # 
+
+# napis = "Litwo! Ojczyzno moja."
+
+# for i in range(0, len(napis)):
+#     print(napis[i], end=" ")
+
+# print()
 
 # for i in napis:
-#     print(i, end="  ")
+#     print(i, end=" ")
 
 
-# example z list
+# print(napis.replace("!", ".").split(sep=". "))
 
-# L = [3, 5, 7, 8, 9, 12, 13, 14, 17]
-# print(L[3])
-# print(L[2:6])
+
+# napis = " Ala ma kota. Kot ma zabawkę."
+
+# print(napis[2])   # a
+# print(napis[2:8]) # a ma k
+# print(len(napis)) # 12
+# print(napis.count("a"))
+
+# print(napis.upper())
+# print(napis.lower())
+# print(napis.capitalize())
+
+# print(napis.strip())  # lstrip rstrip
+# print(napis.replace("a", "-x-"))
+
+#napis = napis.replace(".", "")
+
+# print(napis.split(sep="a"))
+
+
+
+
+
+
+
+# # przypomnienie z list
+
+# L = [2, 3, 5, 8, 13, 21, 22, 23, 24, 25]
+
+# print(L[3])  # 8
+# print(L[3:7]) # 8 - 22
+# print(len(L)) # 10
+# print(L[:5:2]) # 2 5 13
 
