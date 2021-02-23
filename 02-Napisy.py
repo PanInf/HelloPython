980cf91316eb797fda90c965df5aa1cefbb1ab69
PanInfVSC <paninf@paninf.pl>
2021-02-23 10:06:54 +0100

    String ver 1

---
+++ 02-Napisy.py
@@ -0,0 +1,31 @@
+litera = "a"
+print(litera)
+print(ord(litera))
+print(chr(97))
+
+# tekst = " Litwo! Ojczyzno moja. Ty jesteś jak zdrowie."
+
+# print(tekst.upper())  # MATURA
+# print(tekst.lower())  # MATURA
+# print(tekst.capitalize())
+# print(tekst.strip())  #lstrip rstrip  MATURA
+# print(tekst.replace("a", "-xxx-"))
+# print(tekst.split())  # MATURA
+# print(tekst.split(sep="."))
+# print(tekst.replace("!", ".").split(sep="."))
+
+# napis = "Ala ma kota." 
+# print(napis[2])
+# print(napis[2:8])
+# print(len(napis))
+
+# for i in napis:
+#     print(i, end="  ")
+
+
+# example z list
+
+# L = [3, 5, 7, 8, 9, 12, 13, 14, 17]
+# print(L[3])
+# print(L[2:6])
+
