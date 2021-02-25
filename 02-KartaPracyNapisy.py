2f4e4e4b03f05afe32c36c64f8b9807eb82d76bf
PanInfVSC <paninf@paninf.pl>
2021-02-25 10:52:44 +0100

    Adding KartaPracyNapisy.py

---
+++ 02-KartaPracyNapisy.py
@@ -0,0 +1,52 @@
+# KARTA PRACY NAPISY - Powodzenia!
+
+# 1. Generowanie napisów (wielkie litery A-Z)
+
+# a) napisz skrytp, który wygeneruje 10 dowolnych
+#    trzyliterowych słów. Wypisz je na ekran. 
+
+# b) wiedząc, że ludzkie DNA to ciąg 4 białek A, G, C, T stwórz losowy 
+#    łańcuch DNA składający się z 4000 znaków. 
+#    Sprawdź czy gdzieś udało się
+#    uzyskać 4 takie same literki koło siebie
+
+
+# 2. Napisy z klawiatury (wielkie litery A-Z)
+
+# a) sprawdź czy wpisane przez usera imię jest żeńskie
+
+# b) popraw błąd. Załóż, że user wpisuje jakieś słowo z "Ó", 
+#  a powinien z "U". Popraw ten błąd i wypisz poprawne słowo na ekran
+
+# c) usuń wszystkie spacje z napisu-zdania wpisanego przez usera
+
+# d) niech user wpisze date w formacie D-M-R 
+#    Wypisz tą datę w formacie R-M-D 
+
+# 3. Praca z plikiem loko.txt (wielkie litery bez polskich znaków)
+
+# a) policz wiersze w pliku
+
+# b) policz słowa w pliku
+
+# c) policz spacje w pliku
+
+# d) policz literki "A" w parzystych wierszach
+
+# e) Których literek jest więcej, "Z", czy "W"?
+
+# f) podaj ilość wierszy o liczbie słów większej niż 8
+
+# g) podaj ilość wierszy rozpoczynających 
+#    i kończących się na tą samą literę
+
+# h) podaj ilość słów rozpoczynających 
+#    i kończących się na tą samą literę
+
+# i) wypisz tekst małymi literami
+
+# j) policz w ilu miejscach pliku występują 
+#    dwa takie same znaki koło siebie
+
+# k*) policz w ilu miejscach pliku występują dwie
+#     takie same literki koło siebie (nawet jeśli oddziela je spacja)
